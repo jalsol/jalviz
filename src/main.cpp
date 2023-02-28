87b9803916c025ccedf2d33c9248ae72bee59b62
jalsol <jalsol@pm.me>
2023-02-28 14:46:21 +0700

    Remove raylib-cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,27 @@
-#include <iostream>
+#include "constants.hpp"
+#include "raygui.h"
+#include "scene/menu_scene.hpp"
+#include "scene/stack_scene.hpp"
 
-int main() { std::cout << "hi\n"; }
+int main() {
+    InitWindow(constants::scene_width, constants::scene_height,
+               "VisuAlgo.net clone in C++ by @jalsol");
+    SetTargetFPS(constants::frames_per_second);
+
+    GuiLoadStyle("./data/bluish_open_sans.rgs");
+
+    scene::StackScene scene;
+
+    while (!WindowShouldClose()) {
+        scene.interact();
+
+        BeginDrawing();
+        {
+            ClearBackground(WHITE);
+            scene.render();
+        }
+        EndDrawing();
+    }
+
+    return 0;
+}
--- src/scene/menu_scene.cpp
+++ src/scene/menu_scene.cpp
@@ -4,7 +4,6 @@
 
 #include "constants.hpp"
 #include "raygui.h"
-#include "raylib-cpp.hpp"
 #include "utils.hpp"
 
 namespace scene {
