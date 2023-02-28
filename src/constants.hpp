5d8fa31a977071c2b4ff8f2a11b82a47d8652757
jalsol <jalsol@pm.me>
2023-02-28 10:39:00 +0700

    Add constants

---
+++ src/constants.hpp
@@ -0,0 +1,17 @@
+#ifndef CONSTANTS_HPP_
+#define CONSTANTS_HPP_
+
+#include <cstddef>
+
+namespace constants {
+
+constexpr int scene_width = 1366;
+constexpr int scene_height = 768;
+constexpr int frames_per_second = 30;
+
+constexpr int navbar_height = 64;
+constexpr int ani_speed = 8;
+
+}  // namespace constants
+
+#endif  // CONSTANTS_HPP_
