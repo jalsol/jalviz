5da7733d7738d07aafc5e8f188ee46b9296dde54
jalsol <jalsol@pm.me>
2023-03-01 11:54:17 +0700

    Modify constant values

--- src/constants.hpp
+++ src/constants.hpp
@@ -1,8 +1,6 @@
 #ifndef CONSTANTS_HPP_
 #define CONSTANTS_HPP_
 
-#include <cstddef>
-
 namespace constants {
 
 constexpr int scene_width = 1366;
@@ -12,10 +10,10 @@ constexpr int frames_per_second = 30;
 constexpr int navbar_height = 64;
 constexpr int ani_speed = 8;
 
-constexpr int text_buffer_size = 128;
+constexpr int text_buffer_size = 512;
 
 constexpr int min_val = 0;
-constexpr int max_val = 99;
+constexpr int max_val = 999;
 
 }  // namespace constants
 
