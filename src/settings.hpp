5a57057d060e88e51351736c017f6889679ddf67
jalsol <jalsol@pm.me>
2023-05-01 02:11:59 +0700

    Add doxygen comments to other files

--- src/settings.hpp
+++ src/settings.hpp
@@ -7,9 +7,19 @@
 
 #include "raylib.h"
 
+/**
+ * @brief The settings
+ */
 class Settings {
 public:
+    /**
+     * @brief The number of colors
+     */
     static constexpr int num_color = 9;
+
+    /**
+     * @brief The default colors
+     */
     static constexpr std::array<unsigned, num_color> default_color{{
         0x00000000,
         0x82828200,
@@ -22,23 +32,67 @@ public:
         0xf5f5f500,
     }};
 
-private:
-    Settings() = default;
-    std::array<Color, num_color> m_colors{};
-
-public:
+    /**
+     * @brief Deleted copy constructor
+     */
     Settings(const Settings&) = delete;
+
+    /**
+     * @brief Deleted move constructor
+     */
     Settings(Settings&&) = delete;
+
+    /**
+     * @brief Deleted copy assignment operator
+     */
     Settings& operator=(const Settings&) = delete;
+
+    /**
+     * @brief Deleted move assignment operator
+     */
     Settings& operator=(Settings&&) = delete;
+
+    /**
+     * @brief Destructor
+     */
     ~Settings();
 
+    /**
+     * @brief Gets the instance
+     * @return Settings& The instance
+     */
     static Settings& get_instance();
 
+    /**
+     * @brief Gets the color
+     * @param index The index of the color
+     * @return Color& The color
+     */
     Color& get_color(std::size_t index);
+
+    /**
+     * @brief Gets the color
+     * @param index The index of the color
+     * @return Color The color
+     */
     Color get_color(std::size_t index) const;
 
+    /**
+     * @brief Saves the settings to a file
+     * @param path The path to the file
+     */
     void save_to_file(const std::string& path);
+
+private:
+    /**
+     * @brief Private constructor
+     */
+    Settings() = default;
+
+    /**
+     * @brief The colors
+     */
+    std::array<Color, num_color> m_colors{};
 };
 
 #endif  // SETTINGS_HPP_
--- src/utils.hpp
+++ src/utils.hpp
@@ -8,13 +8,42 @@
 #include "core/deque.hpp"
 #include "raylib.h"
 
+/**
+ * @brief The utility functions
+ */
+
 namespace utils {
 
+/**
+ * @brief Draws text with custom font size and spacing
+ *
+ * @param text The drawn text
+ * @param pos The position of the text
+ * @param color The color of the text
+ * @param font_size The font size of the text
+ * @param spacing The spacing of the text
+ */
 void DrawText(const char* text, Vector2 pos, Color color, float font_size,
               float spacing);
 
+/**
+ * @brief Measures the text with custom font size and spacing
+ *
+ * @param text The measured text
+ * @param font_size The font size of the text
+ * @param spacing The spacing of the text
+ * @return Vector2 The size of the text
+ */
 Vector2 MeasureText(const char* text, float font_size, float spacing);
 
+/**
+ * @brief Get a random number in the range [low, high]
+ *
+ * @tparam T Integral type
+ * @param low The lower bound
+ * @param high The upper bound
+ * @return T The random number
+ */
 template<typename T>
 T get_random(T low, T high) {
     if (low > high) {
@@ -27,17 +56,51 @@ T get_random(T low, T high) {
     return dist(prng);
 }
 
+/**
+ * @brief Extracts integers from a string separated by commas
+ *
+ * @param str The string
+ * @return core::Deque<int> The extracted data
+ */
 core::Deque<int> str_extract_data(
     char str[constants::text_buffer_size]);  // NOLINT
 
+/**
+ * @brief Checks if a value is in range [min_val, max_val]
+ *
+ * @param num The value
+ * @retval true The value is in range
+ * @retval false The value is not in range
+ */
 bool val_in_range(int num);
 
+/**
+ * @brief Tells the compiler that this branch is unreachable
+ */
 void unreachable();
 
+/**
+ * @brief Splits a string into tokens
+ *
+ * @param str The string
+ * @param delim The delimiter
+ * @param save_ptr The save pointer
+ * @return char* The token
+ */
 char* strtok(char* str, const char* delim, char** save_ptr);
 
+/**
+ * @brief Converts a hex string to a color
+ * @param str The string
+ */
 Color color_from_hex(const std::string& hex);
 
+/**
+ * @brief Returns the color of the text based on the background color
+ * @param color The background color
+ * @retval BLACK The text color is black
+ * @retval WHITE The text color is white
+ */
 Color adaptive_text_color(Color bg_color);
 
 }  // namespace utils
